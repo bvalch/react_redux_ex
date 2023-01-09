0037d78464add403f3a4cd14ba0ff891383e540c
bvalch <bvalch@gmail.com>
2023-01-09 14:25:02 +0000

    set up redux hooks and counter buttons tied to them

--- react_redux/src/app/store.js
+++ react_redux/src/app/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+//importing the reducer from counterSlice
+import counterReducer from "../features/counter/counterSlice"
 
 
 
 export const store = configureStore({
-  reducer: {},
+    //reducer goes here
+  reducer: {
+    counter: counterReducer,
+  },
 });
